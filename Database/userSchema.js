9a25fe1aa4f00ce96b5f6a7a5e617360e5797914
Piyush Gupta <95485177+piyush-gupta123@users.noreply.github.com>
2022-07-16 16:25:33 +0530

    Add files via upload

---
+++ Database/userSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const currentSchema = new mongoose.Schema({
+    Name: {type:String, required:true},
+    dob: {type:Date, default:Date.now() ,required: true},
+    contact: {type:Number, required: true},
+    gender:{type:String,required:true},
+    email:{type:String, required: true,unique:true},
+    state: {type:String, required: true},
+    select: {type:String,required:true}
+},
+{
+    collection:'data'
+});
+
+module.exports = mongoose.model('USER',currentSchema);
